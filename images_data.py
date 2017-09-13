a9540f8d99a694325235a486df6b455a25e9476e
Novosad Victor <novosad_msu@mail.ru>
2017-09-13 07:29:14 +0300

    img_data is example, need to save weigts somewhere

---
+++ images_data.py
@@ -0,0 +1,47 @@
+from importlib import reload
+import CNNClass
+import csv
+
+
+def mod(m, n):
+    return m - (m // n) * n
+
+
+answers_list = [[0 for i in range(10)] for j in range(42000)]
+image_list = [[0 for j in range(784)] for i in range(42000)]
+
+with open('train.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    i = 0
+    for row in reader:
+        if row[0] == 'label':
+            continue
+        answers_list[i][int(row[0])] = 0.5
+        image_list[i] = row[1:]
+        i += 1
+
+images_list = []
+for s in range(len(image_list)):
+    image = [[0 for j in range(28)] for i in range(28)]
+    for k in range(len(image_list[s])):
+        j = mod(k, 28)
+        i = (k - j) // 28
+        image[i][j] = int(image_list[s][k]) / 255
+    images_list.append(image)
+
+names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+image_size = 28
+filter_size = 5
+filters_quantity = 10
+pooling_size = 2
+hidden_1_size = 600
+hidden_2_size = 400
+n = 5
+cnn = CNNClass.CNNSearch(
+    image_size, names, filter_size, filters_quantity, pooling_size, hidden_1_size, hidden_2_size
+)
+cnn.training_set(images_list, answers_list, n)
+
+
+
+
